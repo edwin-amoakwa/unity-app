ce0f7fc31cf8b1f8b0d1e2475ea842ce1aea116a
Edwin Kwame Amoakwa <edwin.amoakwa@gmail.com>
2025-09-30 23:41:54 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  baseUrl: 'http://localhost:8080/unity-admin/api/v1',
+  baseUrl: 'api',
   appName: 'Unity App',
   enableDebugMode: true,
   logLevel: 'debug',
