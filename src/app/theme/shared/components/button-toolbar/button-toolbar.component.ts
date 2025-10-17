4cc94f8cd5531b84fe1c6802e522d75998f10006
Edwin Kwame Amoakwa <edwin.amoakwa@gmail.com>
2025-10-17 09:58:44 +0000

    update on groups

--- src/app/theme/shared/components/button-toolbar/button-toolbar.component.ts
+++ src/app/theme/shared/components/button-toolbar/button-toolbar.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'button-toolbar',
   standalone: true,
+  imports: [NgStyle],
   templateUrl: './button-toolbar.component.html',
   styleUrl: './button-toolbar.component.scss'
 })
-export class ButtonToolbarComponent {}
+export class ButtonToolbarComponent {
+  // Controls the horizontal alignment of the toolbar content. Defaults to right.
+  @Input() align: 'left' | 'centre' | 'right' = 'right';
+
+  // Map the align input to the appropriate CSS justify-content value.
+  get justifyContent(): 'flex-start' | 'center' | 'flex-end' {
+    switch (this.align) {
+      case 'left':
+        return 'flex-start';
+      case 'centre': // British spelling as requested
+        return 'center';
+      case 'right':
+      default:
+        return 'flex-end';
+    }
+  }
+}
